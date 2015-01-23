62b5de0684847d8e2301e35cab02762fd8f29a41
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2015-01-23 18:37:28 +0300

    добавлена отправка решения + исправлена ошибка cookie-jar vs cookie

--- main.el
+++ main.el
@@ -3,22 +3,39 @@
   (match-string 1 page))
 
 (defun cf-login (uname psswd remember)
-  (setq cf-response 
+  (setq cf-response
 	(shell-command-to-string
 	 (format "curl --silent --cookie-jar %s '%s://%s/enter'"
 		 cf-cookies-file
 		 cf-proto cf-host)))
   (setq cf-csrf-token (cf-get-csrf-token cf-response))
   (setq cf-response
-	(shell-command-to-string 
-	 (format "curl --location --silent --cookie-jar %s --data 'action=enter&handle=%s&password=%s&remember=%s&csrf_token=%s' '%s://%s/enter'"
-		 cf-cookies-file
+	(shell-command-to-string
+	 (format "curl --location --silent --cookie-jar %s --cookie %s --data 'action=enter&handle=%s&password=%s&remember=%s&csrf_token=%s' '%s://%s/enter'"
+		 cf-cookies-file cf-cookies-file
 		 uname psswd remember cf-csrf-token
 		 cf-proto cf-host))
 	)
   (if (string-match "\"error for__password\"" cf-response)
       '"login: fail [wrong password?]"
     '"login: ok"))
+
+(defun cf-submit(contest problem solution)
+  (setq cf-csrf-token
+	(cf-get-csrf-token
+	 (shell-command-to-string
+	  (format "curl --silent --cookie-jar %s --cookie %s '%s://%s/contest/%s/submit'"
+		  cf-cookies-file cf-cookies-file
+		  cf-proto cf-host contest))))
+  (setq cf-response
+	(shell-command-to-string
+	 (format
+	  "curl --location --silent --cookie-jar %s --cookie %s -F 'csrf_token=%s' -F 'action=submitSolutionFormSubmitted' -F 'submittedProblemIndex=%s' -F 'programTypeId=1' -F 'source=%s' '%s://%s/contest/%s/submit?csrf_token=%s'"
+	  cf-cookies-file cf-cookies-file
+	  cf-csrf-token
+	  problem (url-hexify-string solution)
+	  cf-proto cf-host contest cf-csrf-token
+	  ))))
 
 (defun cf-login-read() 
   (let ((cf-uname (read-string "usename: "))
@@ -30,11 +47,10 @@
 (defun cf-logout ()
   (delete-file cf-cookies-file))
 
-
-
 (progn
   (setq cf-host "codeforces.ru")
   (setq cf-proto "http")
   (setq cf-cookies-file "~/.cf-cookies")
   (cf-logout)
-  (cf-login-read))
+  (cf-login-read)
+  (cf-submit "505" "A" "submit.cc"))
